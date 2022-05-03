90048d066bb7c67bb176690ce1c908743200daba
danybeltran <josuebeltran_2010@hotmail.com>
2022-05-03 17:29:24 -0600

    remove unnecesary files

--- src/lib/http.ts
+++
@@ -1,7 +0,0 @@
-import { createHttpClient, fetcher } from "http-react-fetcher"
-
-export const useApi = fetcher.extend({
-  baseUrl: "",
-})
-
-export const client = createHttpClient(useApi.config.baseUrl)
