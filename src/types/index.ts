ecda28dd2028c9db0af5b7b5c6b7117b1bc5e784
Dany Beltran <josuebeltran_2010@hotmail.com>
2022-05-03 18:30:09 -0600

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,11 +3,3 @@ import { NavigationProp } from "@react-navigation/native"
 export type withNavigation<T = {}> = {
   navigation: NavigationProp<any>
 } & T
-
-export type treatmentType = {
-  nombre: string
-  sintomas: string[]
-  precio: number
-  indicaciones: string[]
-  contraindicaciones: string[]
-}
